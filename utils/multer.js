2272c1f836533f3b8cf7cd4ffe4aee26aa73aeac
CYPRIANCY <akpensesughcyprian@gmail.com>
2025-11-09 22:12:10 +0100

    update

--- utils/multer.js
+++ utils/multer.js
@@ -1,5 +1,5 @@
 import multer from 'multer';
-import { v2 as cloudinary } from 'cloudinary';
+// import { v2 as cloudinary } from 'cloudinary';
 import { CloudinaryStorage } from "multer-storage-cloudinary";
 import cloudinary from "../cloudinary.js";
 
