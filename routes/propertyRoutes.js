7da9e49422f8b504bf786a9a37a88c5086bdf4f0
CYPRIANCY <akpensesughcyprian@gmail.com>
2025-11-09 22:37:46 +0100

    update

--- routes/propertyRoutes.js
+++ routes/propertyRoutes.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { protect } from '../middleware/authMiddleware.js';
-import upload from "../utils/multer.js";
+import {upload} from "../utils/multer.js";
 
 import {
   addFavourite,
